eaafeede9271ccf8c51007f1f62f31de3b46af6f
Salman Nabi <salmannabi183@yahoo.com>
2022-04-22 04:27:48 +0100

    QWP-21 #comment front-end retrieval of data by columns complete

--- src/main/resources/static/scripts/dom.js
+++ src/main/resources/static/scripts/dom.js
@@ -6,6 +6,11 @@ export const inputFuelType = document.querySelector(`#inputFuelType`);
 
 export const readInputId = document.querySelector('#readInputId');
 export const deleteInputId = document.querySelector('#deleteInputId');
+export const readInputMake = document.querySelector(`#readInputMake`);
+export const readInputModel = document.querySelector(`#readInputModel`);
+export const readInputBodyType = document.querySelector(`#readInputBodyType`);
+export const readInputFuelType = document.querySelector(`#readInputFuelType`);
+export const readInputGearbox = document.querySelector(`#readInputGearbox`);
 
 export const listOutput = document.getElementById(`listOutput`);
 
@@ -13,3 +18,8 @@ export const buttonRead = document.querySelector('#buttonRead');
 export const buttonDelete = document.querySelector('#buttonDelete');
 export const buttonCreate = document.querySelector(`#buttonCreate`);
 export const buttonReadAll = document.querySelector(`#buttonReadAll`);
+export const buttonReadMake = document.querySelector(`#buttonReadMake`);
+export const buttonReadModel = document.querySelector(`#buttonReadModel`);
+export const buttonReadBodyType = document.querySelector(`#buttonReadBodyType`);
+export const buttonReadFuelType = document.querySelector(`#buttonReadFuelType`);
+export const buttonReadGearbox = document.querySelector(`#buttonReadGearbox`);
--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -45,6 +45,111 @@ const getSingle = () => {
   });
 }
 
+const getMake = () => {
+  DOM.listOutput.innerHTML = '';
+  let make = DOM.readInputMake.value;
+
+  axios.get(`http://localhost:8080/getByMake/${make}`).then(response => {
+    if (!Array.isArray(response.data)) {
+      writeItem(response.data);
+    } else if (response.data.length == 0) {
+      const child = document.createElement(`li`);
+      child.innerHTML = `Car of make "${make}" does not exist!`;
+      DOM.listOutput.appendChild(child);
+    } else {
+      for (let item of response.data) {
+        writeItem(item);
+      }
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+const getModel = () => {
+  DOM.listOutput.innerHTML = '';
+  let model = DOM.readInputModel.value;
+
+  axios.get(`http://localhost:8080/getByModel/${model}`).then(response => {
+    if (!Array.isArray(response.data)) {
+      writeItem(response.data);
+    } else if (response.data.length == 0) {
+      const child = document.createElement(`li`);
+      child.innerHTML = `Car of model "${model}" does not exist!`;
+      DOM.listOutput.appendChild(child);
+    } else {
+      for (let item of response.data) {
+        writeItem(item);
+      }
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+const getBodyType = () => {
+  DOM.listOutput.innerHTML = '';
+  let bodyType = DOM.readInputBodyType.value;
+
+  axios.get(`http://localhost:8080/getByBodyType/${bodyType}`).then(response => {
+    if (!Array.isArray(response.data)) {
+      writeItem(response.data);
+    } else if (response.data.length == 0) {
+      const child = document.createElement(`li`);
+      child.innerHTML = `Car of body type "${bodyType}" does not exist!`;
+      DOM.listOutput.appendChild(child);
+    } else {
+      for (let item of response.data) {
+        writeItem(item);
+      }
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+const getFuelType = () => {
+  DOM.listOutput.innerHTML = '';
+  let fuelType = DOM.readInputFuelType.value;
+
+  axios.get(`http://localhost:8080/getByFuelType/${fuelType}`).then(response => {
+    if (!Array.isArray(response.data)) {
+      writeItem(response.data);
+    } else if (response.data.length == 0) {
+      const child = document.createElement(`li`);
+      child.innerHTML = `Car of fuel type "${fuelType}" does not exist!`;
+      DOM.listOutput.appendChild(child);
+    } else {
+      for (let item of response.data) {
+        writeItem(item);
+      }
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+const getGearbox = () => {
+  DOM.listOutput.innerHTML = '';
+  let gearbox = DOM.readInputGearbox.value;
+
+  axios.get(`http://localhost:8080/getByGearbox/${gearbox}`).then(response => {
+    if (!Array.isArray(response.data)) {
+      writeItem(response.data);
+    } else if (response.data.length == 0) {
+      const child = document.createElement(`li`);
+      child.innerHTML = `Car with gearbox type "${gearbox}" does not exist!`;
+      DOM.listOutput.appendChild(child);
+    } else {
+      for (let item of response.data) {
+        writeItem(item);
+      }
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
 const del = () => {
   let id = DOM.deleteInputId.value;
   
@@ -54,7 +159,6 @@ const del = () => {
   axios.delete(`http://localhost:8080/remove/${id}`).then(response => {
     if (response.status == 204) {
       child.innerHTML = `Car with id: ${id} has been deleted!`;
-      console.log("Success");
     }
   }).catch(err => {
     child.innerHTML = `Car entry with id: ${id} doesn't exist!`;
@@ -84,3 +188,8 @@ DOM.buttonCreate.onclick = () => post();
 DOM.buttonRead.onclick = () => getSingle();
 DOM.buttonDelete.onclick = () => del();
 DOM.buttonReadAll.onclick = () => get();
+DOM.buttonReadMake.onclick = () => getMake();
+DOM.buttonReadModel.onclick = () => getModel();
+DOM.buttonReadBodyType.onclick = () => getBodyType();
+DOM.buttonReadFuelType.onclick = () => getFuelType();
+DOM.buttonReadGearbox.onclick = () => getGearbox();
